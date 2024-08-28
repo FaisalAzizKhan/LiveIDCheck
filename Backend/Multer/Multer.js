76bc17c56964217f12c74d59c17b3a2fa4a96e0f
FaisalAzizKhan <faisalazeez69@gmail.com>
2024-08-28 22:10:32 +0500

    F B

---
+++ Backend/Multer/Multer.js
@@ -0,0 +1,14 @@
+const multer = require("multer");
+
+// Multer configuration
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, uniqueSuffix + path.extname(file.originalname))
+    }
+});
+
+const upload = multer({ storage: storage })
